354cf5c562b28fe0125fa13ba3ea980fb44334a3
Samnoon Haider <samnoon.haider@gmail.com>
2019-02-02 12:37:00 -0600

    created game logic

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -7,19 +7,25 @@ var cards = [
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("The user picked " + cardOne);
+var flipCard = function(cardId){
 
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("The user picked " + cardTwo);
+	console.log("User flipped " + cards[cardId]);
 
-if (cardsInPlay.length===2){
-	if(cardsInPlay[0]===cardsInPlay[1]){
-		alert("You found a match!");
+	cardsInPlay.push(cards[cardId]);
+
+	var checkForMatch = function(){
+
+		if (cardsInPlay.length===2){
+			if(cardsInPlay[0]===cardsInPlay[1]){
+				alert("You found a match!");
+			}
+			else{
+				alert("Sorry, try again");
+			}
+		}
 	}
-	else{
-		alert("Sorry, try again");
-	}
+	checkForMatch();
 }
+
+flipCard(0);
+flipCard(2);
