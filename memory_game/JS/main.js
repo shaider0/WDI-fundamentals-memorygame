6664a5a9fa3e1b3453fd9c3f13b6dda7829f7ffc
Samnoon Haider <samnoon.haider@gmail.com>
2019-02-06 08:44:36 -0500

    created game logic

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -1,17 +1,19 @@
 var cards = [
-"queen",
-"queen",
-"king",
-"king"
+{rank: "Queen", suit: "hearts", cardImage: "images/queen-of-hearts.png" },
+{rank: "Queen", suit: "diamonds", cardImage: "images/queen-of-diamonds.png" },
+{rank: "King", suit: "hearts", cardImage: "images/king-of-hearts.png"},
+{rank: "King", suit: "diamonds", cardImage: "images/king-of-diamonds.png"}
 ]
 
 var cardsInPlay = [];
 
 var flipCard = function(cardId){
 
-	console.log("User flipped " + cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 
-	cardsInPlay.push(cards[cardId]);
+	cardsInPlay.push(cards[cardId].rank);
 
 	var checkForMatch = function(){
 
@@ -26,6 +28,3 @@ var flipCard = function(cardId){
 	}
 	checkForMatch();
 }
-
-flipCard(0);
-flipCard(2);
